d245fe7b10132b9c57e32dac0f8a85349c1846df
TJC472 <124094785+TJC47@users.noreply.github.com>
2023-09-30 20:44:46 +0200

    Update autoupdate.py

--- autoupdate.py
+++ autoupdate.py
@@ -16,10 +16,15 @@ def check(filename, url):
             print("File is not up to date!\nUpdating...")
             try:
                 file = open(filename,"w")
-                file.write(text1)
+                file.write("")
+                file.close()
+                file = open(filename,"w")
+                file.writelines(text1.split("\n"))
                 file.close()
                 print(f"{filename} is up to date!")
+                print(text1)
             except:
                 print(f"Could not update {filename}! Is the file in use?")
     except:
         pass
+check("autoupdate.py", "https://raw.githubusercontent.com/TJC47/autoupdate/main/autoupdate.py")
