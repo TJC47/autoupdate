247f90e1047c4e7429a9a7de5e93195b64cf5edb
TJC472 <124094785+TJC47@users.noreply.github.com>
2023-09-30 20:25:56 +0200

    Add files via upload

---
+++ autoupdate.py
@@ -0,0 +1,25 @@
+import requests
+print("This Program is running autoupdate by TJC47")
+def check(filename, url):
+    print(f"Checking {filename} for updates...")
+    try:
+        file = open(filename)
+        text = file.read()
+        file.close
+        text1 = requests.get(url).text
+    except:
+        print("Could not check for updates.  Did you mistype? If not the servers are probably down.")
+    try:
+        if text == text1:
+            print(f"{filename} is up to date!")
+        else:
+            print("File is not up to date!\nUpdating...")
+            try:
+                file = open(filename,"w")
+                file.write(text1)
+                file.close()
+                print(f"{filename} is up to date!")
+            except:
+                print(f"Could not update {filename}! Is the file in use?")
+    except:
+        pass
